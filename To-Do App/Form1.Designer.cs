9e3e180089a781b30b0d204532d0774066686954
MarshallPelissier <pelissierm@spu.edu>
2015-10-28 17:31:58 -0700

    added project
    day form
    
    event class outline

---
+++ To-Do App/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace To_Do_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.trv_Daily_Events = new System.Windows.Forms.TreeView();
+            this.dtp_Day = new System.Windows.Forms.DateTimePicker();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.clm_Events = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clm_Due_Date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_Date = new System.Windows.Forms.TextBox();
+            this.btn_Add_Event = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.eventsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projectsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newEventToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eventListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projectListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(639, 45);
+            this.monthCalendar1.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.monthCalendar1.MaxSelectionCount = 1;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // trv_Daily_Events
+            // 
+            this.trv_Daily_Events.Location = new System.Drawing.Point(15, 145);
+            this.trv_Daily_Events.Name = "trv_Daily_Events";
+            this.trv_Daily_Events.Size = new System.Drawing.Size(585, 440);
+            this.trv_Daily_Events.TabIndex = 2;
+            // 
+            // dtp_Day
+            // 
+            this.dtp_Day.Location = new System.Drawing.Point(639, 219);
+            this.dtp_Day.Name = "dtp_Day";
+            this.dtp_Day.Size = new System.Drawing.Size(227, 20);
+            this.dtp_Day.TabIndex = 4;
+            this.dtp_Day.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clm_Events,
+            this.clm_Due_Date});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(639, 250);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(227, 400);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // clm_Events
+            // 
+            this.clm_Events.Text = "Events";
+            this.clm_Events.Width = 90;
+            // 
+            // clm_Due_Date
+            // 
+            this.clm_Due_Date.Text = "Due Date";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.txt_Date);
+            this.panel1.Location = new System.Drawing.Point(50, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 80);
+            this.panel1.TabIndex = 7;
+            // 
+            // txt_Date
+            // 
+            this.txt_Date.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Date.Location = new System.Drawing.Point(125, 17);
+            this.txt_Date.Name = "txt_Date";
+            this.txt_Date.Size = new System.Drawing.Size(250, 46);
+            this.txt_Date.TabIndex = 0;
+            this.txt_Date.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_Add_Event
+            // 
+            this.btn_Add_Event.Location = new System.Drawing.Point(15, 600);
+            this.btn_Add_Event.Name = "btn_Add_Event";
+            this.btn_Add_Event.Size = new System.Drawing.Size(100, 50);
+            this.btn_Add_Event.TabIndex = 8;
+            this.btn_Add_Event.Text = "New Event";
+            this.btn_Add_Event.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(130, 600);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 50);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "New Project";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.eventsToolStripMenuItem,
+            this.projectsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(884, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // eventsToolStripMenuItem
+            // 
+            this.eventsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newEventToolStripMenuItem,
+            this.eventListToolStripMenuItem});
+            this.eventsToolStripMenuItem.Name = "eventsToolStripMenuItem";
+            this.eventsToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.eventsToolStripMenuItem.Text = "Events";
+            // 
+            // projectsToolStripMenuItem
+            // 
+            this.projectsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newProjectToolStripMenuItem,
+            this.projectListToolStripMenuItem});
+            this.projectsToolStripMenuItem.Name = "projectsToolStripMenuItem";
+            this.projectsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.projectsToolStripMenuItem.Text = "Projects";
+            // 
+            // newEventToolStripMenuItem
+            // 
+            this.newEventToolStripMenuItem.Name = "newEventToolStripMenuItem";
+            this.newEventToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newEventToolStripMenuItem.Text = "New Event";
+            // 
+            // eventListToolStripMenuItem
+            // 
+            this.eventListToolStripMenuItem.Name = "eventListToolStripMenuItem";
+            this.eventListToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.eventListToolStripMenuItem.Text = "Event List";
+            // 
+            // newProjectToolStripMenuItem
+            // 
+            this.newProjectToolStripMenuItem.Name = "newProjectToolStripMenuItem";
+            this.newProjectToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newProjectToolStripMenuItem.Text = "New Project";
+            // 
+            // projectListToolStripMenuItem
+            // 
+            this.projectListToolStripMenuItem.Name = "projectListToolStripMenuItem";
+            this.projectListToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.projectListToolStripMenuItem.Text = "Project List";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 661);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_Add_Event);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.dtp_Day);
+            this.Controls.Add(this.trv_Daily_Events);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TO DO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TreeView trv_Daily_Events;
+        private System.Windows.Forms.DateTimePicker dtp_Day;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader clm_Events;
+        private System.Windows.Forms.ColumnHeader clm_Due_Date;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt_Date;
+        private System.Windows.Forms.Button btn_Add_Event;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eventsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newEventToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eventListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem projectsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newProjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem projectListToolStripMenuItem;
+    }
+}
+
---
+++ To-Do App/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_Do_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CustomFormat();
+        }
+        
+        private void CustomFormat()
+        {
+        dtp_Day.Format = DateTimePickerFormat.Custom;
+        dtp_Day.CustomFormat = "MM/dd/yyyy";
+        //dtp_Due_Date.Format = DateTimePickerFormat.Custom;
+        //dtp_Due_Date.CustomFormat = "MM/dd/yyyy - hh:00 tt";
+
+        }
+        private void btn_Add_Node_Click(object sender, EventArgs e)
+        {
+            trv_Daily_Events.Nodes.Add("project1");
+            trv_Daily_Events.Nodes.Add("project2");
+            trv_Daily_Events.Nodes[0].Nodes.Add("sub");
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            txt_Date.Text = dtp_Day.Value.ToString("MM/dd/yyyy");
+            monthCalendar1.SetDate(dtp_Day.Value);
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            txt_Date.Text = monthCalendar1.SelectionRange.Start.ToString("MM/dd/yyyy");
+            dtp_Day.Value = monthCalendar1.SelectionRange.Start;
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
